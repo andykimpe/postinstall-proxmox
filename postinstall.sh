37016a569326cbd1d7682705fa2a0175cfe24891
Kimpe Andy <andykimpe@gmail.com>
2013-12-30 03:34:06 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,6 @@
 # patch no licence thank tolaris.com
 wget https://raw.github.com/andykimpe/postinstall-proxmox/master/patch -O - | patch -p0
 sed -i "s|deb https://enterprise.proxmox.com/debian wheezy pve-enterprise|#deb https://enterprise.proxmox.com/debian wheezy pve-enterprise|" "/etc/apt/sources.list.d/pve-enterprise.list"
-apt-get update
+apt-get update > /dev/null 2>&1
 apt-get -y dist-upgrade > /dev/null 2>&1
 echo "OK"
