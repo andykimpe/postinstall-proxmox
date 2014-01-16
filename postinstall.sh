8e894868580db6e1c2e970a27a8a4dbed8198e3d
Kimpe Andy <andykimpe@gmail.com>
2014-01-16 05:37:25 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sed -i "s|deb https://enterprise.proxmox.com/debian wheezy pve-enterprise|#deb https://enterprise.proxmox.com/debian wheezy pve-enterprise|" "/etc/apt/sources.list.d/pve-enterprise.list"
-apt-get update > /dev/null 2>&1
-apt-get -y dist-upgrade > /dev/null 2>&1
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y install patch
 # patch no licence thank tolaris.com
 wget https://raw.github.com/andykimpe/postinstall-proxmox/master/patch -qO- | patch -p0
 # patch réseau correctif définir virtio en tant que carte par défaut
