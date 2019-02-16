d7a7d19f8eaab2d2a4d565804529ff1abba49dfe
Kimpe Andy <andykimpe@gmail.com>
2019-02-16 02:43:25 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -10,10 +10,10 @@ apt-get -y dist-upgrade
 # install patch
 apt-get -y install patch
 # patch no licence thank andykimpe for update tested 4.4 working
-auto check dir file pvemanagerlib.js for compatibility old v4.2.x and 4.4.x and auter update on v4
-pvemanagerlibdir="$(find /usr/share/pve-manager/ -name pvemanagerlib.js)"
-sed -i "s|if (data.status !== 'Active') {|if (false && data.status !== 'Active') {|" "$pvemanagerlibdir"
+wget https://raw.github.com/andykimpe/postinstall-proxmox/master/patch -qO- | patch -p0
 # patch network défine virtio on default network card not updated
 wget https://raw.github.com/andykimpe/postinstall-proxmox/master/patch-virtio -qO- | patch -p0
+systemctl stop pveproxy
+systemctl start pveproxy
 echo "OK"
 exit
